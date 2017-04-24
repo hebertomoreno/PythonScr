6c5c473106684af1219286153fbaeb44d62efd6b
Heberto Moreno Márquez <nikolasmurdock@gmail.com>
2017-04-24 16:36:27 -0500

    Stack and queue exercise

---
+++ solutionforqueueandstack.py
@@ -0,0 +1,13 @@
+from collections import deque
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = deque([])
+    def pushCharacter(self,ch):
+        self.stack.append(ch)
+    def enqueueCharacter(self,ch):
+        self.queue.append(ch)
+    def popCharacter(self):
+    	return self.stack.pop()
+    def dequeueCharacter(self):
+    	return self.queue.popleft()
